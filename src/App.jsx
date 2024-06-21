a1d1eaa1dd36eaef189f5b68b333cb2cef5d6148
nodirbek-zafarovich <soliyevnodirbek001@gmail.com>
2024-06-21 12:36:47 +0500

    master

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import About from './About';
+import Footer from './Footer';
+import Header from './Header';
+import Partners from './Partners';
+import Portfolio from './Portfolio';
+import Services from './Services';
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <About />
+      <Portfolio/>
+      <Services/>
+      <Partners/>
+      <Footer/>
+      
+    </>
+  );
+};
+
+export default App;
